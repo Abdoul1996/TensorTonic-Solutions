1c7eb8ca4c757fb3f9c10b8c4387cc065751039a
Abdoulfatah <43047286+Abdoul1996@users.noreply.github.com>
2026-08-15 16:20:12 -0700

    Solve log-transform

---
+++ log-transform/log-transform.py
@@ -0,0 +1,10 @@
+import math 
+def log_transform(values):
+    """
+    Apply the log1p transformation to each value.
+    """
+    # Write code here
+    result = []
+    for i in range(len(values)):
+        result.append(math.log(1+values[i]))
+    return result
