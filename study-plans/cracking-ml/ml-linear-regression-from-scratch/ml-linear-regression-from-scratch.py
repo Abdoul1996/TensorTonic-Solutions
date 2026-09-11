a9eac4bd28d805ca70409221835157ce5c86ae8f
Abdoulfatah <43047286+Abdoul1996@users.noreply.github.com>
2026-09-10 17:39:36 -0700

    Solve ml-linear-regression-from-scratch

--- study-plans/cracking-ml/ml-linear-regression-from-scratch/ml-linear-regression-from-scratch.py
+++ study-plans/cracking-ml/ml-linear-regression-from-scratch/ml-linear-regression-from-scratch.py
@@ -4,32 +4,32 @@ def linear_regression(X, y, lr, epochs):
     """
     Returns: tuple (weights, bias)
     """
-    X = np.asarray(X, dtype=float)
-    y = np.asarray(y, dtype=float)
-
+    X = np.array(X, dtype=float)
+    y = np.array(y, dtype=float)
+    
     n_samples, n_features = X.shape
+
     w = np.zeros(n_features)
     b = 0.0 
 
     for _ in range(epochs):
-        y_pred = np.dot(X, w) + b # compute the predictions 
-        # Computing the gradients 
-        dw = 2.0/n_samples * np.dot(X.T,(y_pred - y))
-        db = 2.0/n_samples * np.sum(y_pred - y)
-        #  updates the parameters 
-        w = w - lr  * dw
-        b = b - lr * db
-    weights = np.round(w,4)
-    bias = np.round(b,4)
+
+        # computer the predictions 
+        y_pred = np.dot(X,w) + b 
+
+        # compute the gradient descents 
+        errors = y_pred - y 
+        dw = 2/n_samples * np.dot(X.T, errors)
+        db = 2/n_samples * np.sum(errors)
+
+        # update the parameters
+        w -= lr * dw 
+        b -= lr * db
+
+    weights = np.round(w, 4)
+    bias = np.round(b, 4)
 
     return (weights, bias)
-        
-        
-        
-        
-
-        
-        
 
     
     
